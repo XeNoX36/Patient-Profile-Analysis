ccfe12d351cf67edc6de6538b2fc4b4b75cb5912
Sam <114194410+XeNoX36@users.noreply.github.com>
2025-10-17 03:44:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-with open(r"Streamlit\healthcare_app\healthcare_Style.css", "r") as f:
+with open(r"healthcare_Style.css", "r") as f:
     st.markdown(f"""<style>{f.read()}</style>""", unsafe_allow_html=True)
 
 st.set_page_config(page_title="Healthcare Analysis",
@@ -16,3 +16,4 @@ contact_page = st.Page("pages/Contact.py", title="Finance and Insurance")
 dataset_page = st.Page("pages/Dataset.py", title="Data")
 pg = st.navigation([home_page, about_page, contact_page, dataset_page], position="top")
 pg.run()
+

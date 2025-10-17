54ef34380d56a1d5652b95cbc773d8229c73ae1f
Sam <114194410+XeNoX36@users.noreply.github.com>
2025-10-17 04:38:21 +0100

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import plotly.express as px
 from extra import create_widgets
-from create_widgets import approx_val
+from extra import approx_val
 
 # Data Analysis 2
 data, df1, df2, df3, df4, df5, df6, filtered_df, colours, colours2, tot_bill, tot_patient, rev_rate, tot_patient, patient_rate, avg_bill, avg_rate, avg_los, los_rate = create_widgets()
@@ -58,6 +58,7 @@ with col2:
                       xaxis=dict(tickfont=dict(color="#474545"), title=dict(font=dict(color="#474545"))),
                       yaxis=dict(tickfont=dict(color="#474545"), title=dict(font=dict(color="#474545"))))
     st.plotly_chart(fig, use_container_width=True)
+
 
 
 
